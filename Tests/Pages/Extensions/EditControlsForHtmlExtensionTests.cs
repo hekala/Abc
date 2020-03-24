9e240406a4c10878f742ba18bac3a4f5bdc0a23d
Helen Kalamäe <hekala@ttu.ee>
2020-03-24 11:42:49 +0200

    40.loengu commit (mockid)

--- Tests/Pages/Extensions/EditControlsForHtmlExtensionTests.cs
+++ Tests/Pages/Extensions/EditControlsForHtmlExtensionTests.cs
@@ -1,4 +1,8 @@
-﻿using Abc.Pages.Extensions;
+﻿using System.Collections.Generic;
+using Abc.Facade.Quantity;
+using Abc.Pages.Extensions;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Abc.Tests.Pages.Extensions
@@ -11,7 +15,15 @@ namespace Abc.Tests.Pages.Extensions
         [TestMethod]
         public void EditControlsForTest()
         {
-            Assert.Inconclusive();
+            var obj = new htmlHelperMock<UnitView>().EditControlsFor(x => x.MeasureId);
+            Assert.IsInstanceOfType(obj, typeof(HtmlContentBuilder));
+        }
+        [TestMethod]
+        public void HtmlStringsTest()
+        {
+            var expected = new List<string> {"<div", "LabelFor", "EditorFor", "ValidationMessageFor", "</div"};
+            var actual = EditControlsForHtmlExtension.htmlStrings(new htmlHelperMock<MeasureView>(),x=>x.ValidFrom);
+            TestHtml.Strings(actual, expected);
         }
     }
 }
