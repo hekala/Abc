fe91e0e004608d0b596e90dd6a41046d143e69ba
Helen Kalamäe <hekala@ttu.ee>
2020-03-25 14:36:07 +0200

    45 ja 46 loeng

---
+++ Data/Quantity/CommonTermData.cs
@@ -0,0 +1,11 @@
+﻿using Abc.Data.Common;
+
+namespace Abc.Data.Quantity
+{
+    public abstract class CommonTermData: PeriodData
+    {
+        public string MasterId { get; set; } //nt see masterid hoiab kiiruse id
+        public string TermId { get; set; } 
+        public int Power { get; set; } //aste, kas uhik mida selle termiga naidatakse on astmes 1 jne.. 
+    }
+}
---
+++ Tests/Data/Quantity/CommonTermDataTests.cs
@@ -0,0 +1,32 @@
+﻿using Abc.Data.Common;
+using Abc.Data.Quantity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Abc.Tests.Data.Quantity
+{
+    [TestClass]
+    public class CommonTermDataTests: AbstractClassTests<CommonTermData, PeriodData>
+    {
+        private class testClass: CommonTermData { }
+
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            base.TestInitialize();
+            obj = new testClass();
+        }
+
+        [TestMethod] public void MasterIdTest()
+        {
+            isNullableProperty(()=>obj.MasterId, x=> obj.MasterId=x); //1) funktsioon 2) saab x vaartuseks, masterid saab selleks vaartuseks
+        }
+        [TestMethod] public void TermIdTest()
+        {
+            isNullableProperty(()=>obj.TermId, x=> obj.TermId=x);
+        }
+        [TestMethod] public void PowerTest()
+        {
+            isProperty(()=>obj.Power, x=> obj.Power=x);
+        }
+    }
+}
