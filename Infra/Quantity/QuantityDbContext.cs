deedd7fc626ebe95adab9fa5dd744a42ba09ffb4
Helen Kalamäe <hekala@ttu.ee>
2020-04-04 13:58:51 +0300

    53.loeng

--- Infra/Quantity/QuantityDbContext.cs
+++ Infra/Quantity/QuantityDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Abc.Data.Quantity;
+using Abc.Domain.Quantity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Abc.Infra.Quantity
@@ -14,7 +15,8 @@ namespace Abc.Infra.Quantity
         public DbSet<UnitData> Units { get; set; }
         public DbSet<SystemOfUnitsData> SystemsOfUnits { get; set; }
         public DbSet<UnitFactorData> UnitFactors { get; set; }
-
+        public DbSet<MeasureTermData> MeasureTerms { get; set; }
+        public DbSet<UnitTermData> UnitTerms { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder) //loob tabelid
         {
@@ -24,10 +26,13 @@ namespace Abc.Infra.Quantity
 
         public static void InitializeTables(ModelBuilder builder)
         {
+            if (builder is null) return;
             builder.Entity<MeasureData>().ToTable(nameof(Measures));
             builder.Entity<UnitData>().ToTable(nameof(Units));
             builder.Entity<SystemOfUnitsData>().ToTable(nameof(SystemsOfUnits));
             builder.Entity<UnitFactorData>().ToTable(nameof(UnitFactors)).HasKey(x=>new{x.UnitId, x.SystemOfUnitsId});
+            builder.Entity<MeasureTermData>().ToTable(nameof(MeasureTerms)).HasKey(x=>new{x.MasterId, x.TermId});
+            builder.Entity<UnitTermData>().ToTable(nameof(UnitTerms)).HasKey(x=>new{x.MasterId, x.TermId});
         }
     }
 }
--- Tests/Infra/Quantity/MeasureRepositoryTests.cs
+++ Tests/Infra/Quantity/MeasureRepositoryTests.cs
@@ -12,14 +12,27 @@ namespace Abc.Tests.Infra.Quantity
     [TestClass]
     public class MeasureRepositoryTests : RepositoryTests<MeasuresRepository, Measure, MeasureData>
     {
+        private QuantityDbContext db;
+        private int count;
         [TestInitialize] public override void TestInitialize()
         {
             base.TestInitialize(); //kasutab inmemorydb extensionit, mis on malus!
             var options = new DbContextOptionsBuilder<QuantityDbContext>().
                 UseInMemoryDatabase("TestDb").Options;
-            var c = new QuantityDbContext(options);
-            obj = new MeasuresRepository(c);
+            db = new QuantityDbContext(options);
+            obj = new MeasuresRepository(db);
+            count = GetRandom.UInt8(20, 40);
+            foreach (var p in db.Measures)
+                db.Entry(p).State = EntityState.Deleted;
+            addItems();
         }
+
+        private void addItems()
+        {
+            for (var i = 0; i < count; i++)
+                obj.Add(new Measure(GetRandom.Object<MeasureData>())).GetAwaiter();
+        }
+
         protected override Type getBaseType()
         {
             return typeof(UniqueEntityRepository<Measure, MeasureData>);
@@ -27,7 +40,9 @@ namespace Abc.Tests.Infra.Quantity
 
         protected override void testGetList()
         {
-            Assert.Inconclusive();
+            obj.PageIndex = GetRandom.Int32(2, obj.TotalPages-1);
+            var l = obj.Get().GetAwaiter().GetResult();
+            Assert.AreEqual(obj.PageSize, l.Count);
         }
 
         protected override string getId(MeasureData d) => d.Id;
